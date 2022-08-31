ea1e04269276007a687d5db7032b84e6f574f53f
Guangrong Qin <guangrong86@gmail.com>
2022-08-31 00:37:35 -0700

    revise dir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Author: Guangrong_Qin
-#Date: 2022.08.24
+#Date: 2022.08.31
 
 import json
 import sys
@@ -8,8 +8,7 @@ from BigGIM_Parser import *
 
 def main():
     counter = 0
-    directory = "/Users/guangrong/Documents/GitHub_project/BigGIM_Parser/Input_Table/"
-    config_file = open(directory+"config.file.txt")
+    config_file = open("./Input_Table/config.file.txt")
     for line in config_file.readlines():
         #print(line)
         if line.startswith("#") == False and len(line)>0:
@@ -18,7 +17,7 @@ def main():
             label      = line.split(',')[2].strip()
 
             if label == "ON":
-                for row in load_tsv_data(directory+filename,date_label):
+                for row in load_tsv_data(filename,date_label):
                     print(json.dumps(row, sort_keys=True, indent=2))
                     counter += 1
     
